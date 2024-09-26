0ac6663b859e5afc7105b422af89c84195eb4998
bgd <trifan.bogdan09@yahoo.com>
2024-09-26 13:06:08 +0300

    main.rs

---
+++ target/debug/deps/learn_rust-ffb0c9e461921b40.d
@@ -0,0 +1,5 @@
+/home/bogdan/Desktop/learn-rust/target/debug/deps/learn_rust-ffb0c9e461921b40: src/main.rs
+
+/home/bogdan/Desktop/learn-rust/target/debug/deps/learn_rust-ffb0c9e461921b40.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/learn-rust.d
@@ -0,0 +1,1 @@
+/home/bogdan/Desktop/learn-rust/target/debug/learn-rust: /home/bogdan/Desktop/learn-rust/src/main.rs
